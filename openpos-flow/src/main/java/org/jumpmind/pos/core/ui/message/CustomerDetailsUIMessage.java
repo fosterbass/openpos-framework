5fca40f87252e25b0ffe76ec5278b878b906a17b
chenson42 <chenson42@gmail.com>
2021-07-26 17:01:14 -0400

    Add missing field

--- openpos-flow/src/main/java/org/jumpmind/pos/core/ui/message/CustomerDetailsUIMessage.java
+++ openpos-flow/src/main/java/org/jumpmind/pos/core/ui/message/CustomerDetailsUIMessage.java
@@ -49,6 +49,7 @@ public class CustomerDetailsUIMessage extends UIMessage {
     private String loyaltyIcon;
     private String loyaltyNumberIcon;
     private String locationIcon;
+    private String checkMarkIcon;
     private String memberIcon;
     private String nonMemberIcon;
     private String expiredIcon;
