eef51e23eaa85ddda21ddb582b00d2e30ea497e9
Klementina S. Chirico <kstojanovska@jumpmind.com>
2021-07-27 16:59:30 -0400

    Feature/dml with where and optional where clauses master (#1493)
    * Create abstract class AbstractSqlTemplate
    
    * update QueryTemplate to extend AbstractSqlTemplate and remove duplicate fields
    
    * fix formattin
    
    * update DmlTemplate to extend AbstractSqlTemplate and update generateSql to handle where and optionalWhere on dml
    
    * update DmlTemplates to handle where and optionalWhereClauses
    
    * formatting
    
    * Create DmlTemplateTest
    
    * update persist-test-dml.yml with additional dmls and add DBSessionDmlTest.java
    
    * Support named parameters in DML (#1426)
    
    Co-authored-by: mmichalek <mmichalek@jumpmind.com>

--- openpos-persist/src/main/java/org/jumpmind/pos/persist/impl/QueryTemplate.java
+++ openpos-persist/src/main/java/org/jumpmind/pos/persist/impl/QueryTemplate.java
@@ -16,47 +16,24 @@ import org.jumpmind.pos.persist.PersistException;
 import org.jumpmind.pos.persist.Query;
 import org.jumpmind.pos.persist.SqlStatement;
 import org.jumpmind.pos.util.model.AbstractTypeCode;
-import org.springframework.util.Assert;
 
-public class QueryTemplate implements Cloneable {
+public class QueryTemplate extends AbstractSqlTemplate implements Cloneable {
 
-    private String name;
     private String select;
-    private List<String> optionalWhereClauses;
     private String groupBy;
-    private String where;
     private String orderBy;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 
     public void setSelect(String select) {
         this.select = select;
     }
-    
+
     public String getSelect() {
         return select;
     }
 
-    public boolean hasSelect()  { return (this.select != null); }
-
-    public List<String> getOptionalWhereClauses() {
-        if (optionalWhereClauses == null) {
-            optionalWhereClauses = new ArrayList<>();
-        }
-        return optionalWhereClauses;
+    public boolean hasSelect() {
+        return (this.select != null);
     }
-
-    public void setOptionalWhereClauses(List<String> optionalWhereClauses) {
-        this.optionalWhereClauses = optionalWhereClauses;
-    }
-
-    public boolean hasOptionalWhereClauses()  { return !getOptionalWhereClauses().isEmpty(); }
 
     public QueryTemplate optionalWhere(String optionalWhere) {
         this.getOptionalWhereClauses().add(optionalWhere);
@@ -71,17 +48,9 @@ public class QueryTemplate implements Cloneable {
         this.groupBy = groupBy;
     }
 
-    public boolean hasGroupBy()  { return (this.groupBy != null); }
-
-    public String getWhere() {
-        return where;
+    public boolean hasGroupBy() {
+        return (this.groupBy != null);
     }
-
-    public void setWhere(String where) {
-        this.where = where;
-    }
-
-    public boolean hasWhere()  { return (this.where != null); }
 
     public String getOrderBy() {
         return orderBy;
@@ -91,16 +60,18 @@ public class QueryTemplate implements Cloneable {
         this.orderBy = orderBy;
     }
 
-    public boolean hasOrderBy()  { return (this.orderBy != null); }
+    public boolean hasOrderBy() {
+        return (this.orderBy != null);
+    }
 
     public QueryTemplate copy() {
         try {
-            return (QueryTemplate)this.clone();
+            return (QueryTemplate) this.clone();
         } catch (CloneNotSupportedException e) {
             throw new PersistException(e);
         }
     }
-    
+
     public SqlStatement generateSQL(Query<?> query, Object singleParam) {
         Map<String, Object> params = new HashMap<>();
         params.put("*", singleParam);
@@ -111,7 +82,7 @@ public class QueryTemplate implements Cloneable {
         String select = this.getSelect();
         List<String> keys = new ArrayList<>();
 
-        StringSubstitutor literalSubtitution = new StringSubstitutor(new StringLookup() {
+        StringSubstitutor literalSubstitution = new StringSubstitutor(new StringLookup() {
             @Override
             public String lookup(String key) {
                 Object paramValue = params.get(key);
@@ -127,10 +98,10 @@ public class QueryTemplate implements Cloneable {
             }
         });
 
-        String preppedSelectClause = literalSubtitution.replace(select);
+        String preppedSelectClause = literalSubstitution.replace(select);
         preppedSelectClause = sub.replace(preppedSelectClause);
 
-        String preppedWhereClause = literalSubtitution.replace(this.getWhere());
+        String preppedWhereClause = literalSubstitution.replace(this.getWhere());
         preppedWhereClause = sub.replace(preppedWhereClause);
 
         StringBuilder buff = new StringBuilder();
@@ -148,7 +119,7 @@ public class QueryTemplate implements Cloneable {
         boolean firstIncluded = true;
         for (String optionalWhereClause : this.getOptionalWhereClauses()) {
             Set<String> optionalWhereClauseKeys = new LinkedHashSet<>();
-            String preppedOptionalWhereClause = literalSubtitution.replace(optionalWhereClause);
+            String preppedOptionalWhereClause = literalSubstitution.replace(optionalWhereClause);
 
             StringSubstitutor optionalSubstitution = new StringSubstitutor(new StringLookup() {
                 @Override
@@ -182,9 +153,9 @@ public class QueryTemplate implements Cloneable {
                 buff.append(")");
                 keys.addAll(optionalWhereClauseKeys);
             }
-            
+
             if (shouldInclude) {
-               firstIncluded = false;
+                firstIncluded = false;
             }
         }
 
@@ -218,11 +189,11 @@ public class QueryTemplate implements Cloneable {
                             String.format("Missing required query parameter '%s'. Cannot build query: %s", key, sqlStatement.getSql()));
                 }
             } else if (value instanceof Boolean) {
-                boolean bool = (Boolean)value;
+                boolean bool = (Boolean) value;
                 value = bool ? 1 : 0;
                 params.put(key, value);
             } else if (value instanceof AbstractTypeCode) {
-                value = ((AbstractTypeCode)value).value();
+                value = ((AbstractTypeCode) value).value();
                 params.put(key, value);
             }
         }
@@ -231,16 +202,6 @@ public class QueryTemplate implements Cloneable {
         }
         sqlStatement.setParameters(params);
         return sqlStatement;
-    }
-
-    protected String stripWhere(String preppedSelectClause) {
-        Assert.notNull(preppedSelectClause, "preppedSelectClause must be non-null.");
-        String sqlTrimmed = preppedSelectClause.trim();
-        if (sqlTrimmed.endsWith("WHERE") || sqlTrimmed.endsWith("where")) {
-            return sqlTrimmed.substring(0, sqlTrimmed.length() - "where".length());
-        } else {
-            return sqlTrimmed;
-        }
     }
 
     private void splitTooManyValuesInClause(Query<?> query, Map<String, Object> params, StringBuilder buffer) {
@@ -255,7 +216,6 @@ public class QueryTemplate implements Cloneable {
         }
     }
 
-
     private Map<String, Object> splitInClause(Map.Entry<String, Object> entry, StringBuilder buffer, Query<?> query) {
         Map<String, Object> newParams = new HashMap<>();
         Pattern pattern = Pattern.compile("([\\w.$*@]+\\s+(not\\s+)?in\\s*\\(\\s*)(:" + Pattern.quote(entry.getKey()) + ")(\\s*\\))", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE);
@@ -269,8 +229,7 @@ public class QueryTemplate implements Cloneable {
                 if (indexEntry.getKey() > 0) {
                     if (matcher.group(2) != null) {
                         replacement.append(" AND ");
-                    }
-                    else {
+                    } else {
                         replacement.append(" OR ");
                     }
                 }
@@ -280,7 +239,7 @@ public class QueryTemplate implements Cloneable {
                     replacement.append(matcher.group(3)).append("$").append(parameterIndex);
 
                     newParams.put(entry.getKey() + "$" + parameterIndex, entries.get(i));
-                    if(i != entries.size() - 1) {
+                    if (i != entries.size() - 1) {
                         replacement.append(",");
                     }
                     parameterIndex++;
--- openpos-util/src/main/java/org/jumpmind/pos/util/web/NotFoundException.java
+++ openpos-util/src/main/java/org/jumpmind/pos/util/web/NotFoundException.java
@@ -4,4 +4,19 @@ public class NotFoundException extends RuntimeException {
 
     private static final long serialVersionUID = 1L;
 
+    public NotFoundException() {
+        this("");
+    }
+
+    public NotFoundException(String message) {
+        super(message);
+    }
+
+    public NotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public NotFoundException(Throwable cause) {
+        super(cause);
+    }
 }
