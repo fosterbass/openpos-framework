c6cf081cd8e3057b1b6cc2f8ca529064142e03ce
brandon-lewis <blewis@manifestcorp.com>
2021-09-02 14:07:26 -0400

    Updated DynamicBaconStrip to allow for left button/icon action overriding (#1575)

--- openpos-client-libs/projects/openpos-client-core-lib/src/lib/shared/screen-parts/dynamic-bacon-strip/dynamic-bacon-strip.component.ts
+++ openpos-client-libs/projects/openpos-client-core-lib/src/lib/shared/screen-parts/dynamic-bacon-strip/dynamic-bacon-strip.component.ts
@@ -1,10 +1,9 @@
-import {Component, Injector, Input} from '@angular/core';
+import {Component, EventEmitter, Injector, Input, Output} from '@angular/core';
 import { DynamicBaconStripInterface } from './dynamic-bacon-strip.interface';
 import { ScreenPart } from '../../decorators/screen-part.decorator';
 import { ScreenPartComponent } from '../screen-part';
 import { Observable } from 'rxjs';
 import { OpenposMediaService, MediaBreakpoints } from '../../../core/media/openpos-media.service';
-import {BaconStripInterface} from "../bacon-strip/bacon-strip.interface";
 
 
 @ScreenPart({
@@ -19,6 +18,29 @@ export class DynamicBaconStripComponent extends ScreenPartComponent<DynamicBacon
 
   @Input()
   hideLogo: boolean;
+
+  /**
+   * A flag indicating whether the behavior of the left ActionItem button/icon on the baconstrip should be ignored
+   * when the button/icon is clicked.
+   *
+   * If this ever evaluates to true, you should also have an actionOverride specified.
+   * @see actionOverride
+   */
+  @Input()
+  actionOverrideFlag: boolean = false;
+
+  /**
+   * The function that is called when this baconstrip component's ActionItem button/icon is clicked if the
+   * actionOverrideFlag is true.
+   * <hr/>
+   * <b>Warning:</b> The baconstrip action provided by the server state is typically a “Back” action or something else
+   * to be handled by the state. If you are not careful, overriding this baconstrip action may cause the app to get
+   * stuck on this screen without some other way of triggering an action to be handled by the server state.
+   * <hr/>
+   * @see actionOverrideFlag
+   */
+  @Output()
+  actionOverride: EventEmitter<any> = new EventEmitter<any>();
 
   isMobile: Observable<boolean>;
 
@@ -37,7 +59,9 @@ export class DynamicBaconStripComponent extends ScreenPartComponent<DynamicBacon
   screenDataUpdated() { }
 
   handleClick() {
-    if(this.screenData.actions && this.screenData.actions.length === 1) {
+    if(this.actionOverrideFlag && this.actionOverride) {
+      this.actionOverride.emit();
+    } else if (this.screenData.actions && this.screenData.actions.length >= 1) {
       this.doAction(this.screenData.actions[0]);
     }
   }
