17d66a4b451cbc9d7e86534c81783fb2ed908a02
stojankz <kstojanovska@jumpmind.com>
2021-09-09 19:52:21 -0400

    Fix legendPosition

--- openpos-flow/src/main/java/org/jumpmind/pos/core/ui/data/UILegendPosition.java
+++ openpos-flow/src/main/java/org/jumpmind/pos/core/ui/data/UILegendPosition.java
@@ -1,8 +1,6 @@
 package org.jumpmind.pos.core.ui.data;
 
 public final class UILegendPosition {
-    public static String RIGHT = "Right";
-    public static String LEFT = "Left";
-    public static String BOTTOM = "Bottom";
-    public static String TOP = "Top";
+    public static String RIGHT = "right";
+    public static String BELOW = "below";
 }
