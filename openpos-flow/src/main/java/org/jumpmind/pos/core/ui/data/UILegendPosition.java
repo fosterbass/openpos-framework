36f2ebae8c1cc33ffa997a1c9f6a08abcee066a1
stojankz <kstojanovska@jumpmind.com>
2021-09-09 16:38:07 -0400

    Fix consistent string in caps

--- openpos-flow/src/main/java/org/jumpmind/pos/core/ui/data/UILegendPosition.java
+++ openpos-flow/src/main/java/org/jumpmind/pos/core/ui/data/UILegendPosition.java
@@ -3,6 +3,6 @@ package org.jumpmind.pos.core.ui.data;
 public final class UILegendPosition {
     public static String RIGHT = "Right";
     public static String LEFT = "Left";
-    public static String Bottom = "Bottom";
+    public static String BOTTOM = "Bottom";
     public static String TOP = "Top";
 }
