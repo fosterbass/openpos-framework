6803033d549ee05ef2895f9ff93b9ccc9ede29d0
stojankz <kstojanovska@jumpmind.com>
2021-09-09 20:46:44 -0400

    add NoArgsConstructor back

--- openpos-flow/src/main/java/org/jumpmind/pos/core/ui/data/UIGraphSingleDataPoint.java
+++ openpos-flow/src/main/java/org/jumpmind/pos/core/ui/data/UIGraphSingleDataPoint.java
@@ -3,12 +3,14 @@ package org.jumpmind.pos.core.ui.data;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
 @Data
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class UIGraphSingleDataPoint implements Serializable {
     private static final long serialVersionUID = 1L;
 
