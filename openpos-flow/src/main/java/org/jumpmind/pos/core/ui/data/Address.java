f338dc7d8e6060b10b2bf3a6b0a19fe3a7252b06
Vodianytskyi Vladislav <85353723+VodianytskyiVI@users.noreply.github.com>
2021-09-17 17:00:15 +0300

    Fix bill to address line2 displaying (#1598)
    Co-authored-by: Vodianytskyi Vladislav <vodianytsv@ae.com>

--- openpos-flow/src/main/java/org/jumpmind/pos/core/ui/data/Address.java
+++ openpos-flow/src/main/java/org/jumpmind/pos/core/ui/data/Address.java
@@ -5,20 +5,19 @@ import lombok.Builder;
 import lombok.Data;
 
 import java.io.Serializable;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class Address implements Serializable {
     private String attention;
     private String address;
+    private String address2;
     private String city;
     private String state;
     private String postalCode;
-
-    public Address() {
-
-    }
 
     public Address(String address, String city, String state, String postalCode) {
         this.address = address;
