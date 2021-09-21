7463e45dbb172d1fac2872025703e2bd1562d8be
Jason Mihalick <jrmihalick@users.noreply.github.com>
2021-09-21 17:38:20 -0400

    Add locationData to the ClientContext (#1613)
    * PDPOS-6452: Add locationData to the ClientContext
    
    - Client location data was not available for retail calculations for the first item added to the transaction in Commerce. This is because the TransMgmtClient handles setting the TransLocationModel on the transaction, but not until after the transaction has already been created and has the first item added to it.
    - This adds the LocationData onto the ClientContext as json string so that endpoints have access to the LocationData.
    - Moved the LocationData class to openpos-util so that consumers do not have to have a dependency on openpos-flow, where it was previously located.
    
    * PDPOS-6452: Address PR feedback, make ObjectMapper instance static/transient
    
    * PDPOS-6452: Address PR feedback, make ObjectMapper instance static/transient

--- openpos-flow/src/main/java/org/jumpmind/pos/core/service/ILocationListener.java
+++ openpos-flow/src/main/java/org/jumpmind/pos/core/service/ILocationListener.java
@@ -1,5 +1,7 @@
 package org.jumpmind.pos.core.service;
 
+import org.jumpmind.pos.util.model.LocationData;
+
 public interface ILocationListener {
 
     public void locationChanged(LocationData locationData);
