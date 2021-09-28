48ddca980eb6b5204c355c9e6d549be1f1e6d08c
Vodianytskyi Vladislav <85353723+VodianytskyiVI@users.noreply.github.com>
2021-09-28 16:42:48 +0300

    Change GiftReceiptUnmaskCodeDialog fields (#1628)
    Co-authored-by: Vodianytskyi Vladislav <vodianytsv@ae.com>

--- openpos-flow/src/main/java/org/jumpmind/pos/core/ui/GiftReceiptUnmaskCodeDialogUIMessage.java
+++ openpos-flow/src/main/java/org/jumpmind/pos/core/ui/GiftReceiptUnmaskCodeDialogUIMessage.java
@@ -1,11 +1,13 @@
 package org.jumpmind.pos.core.ui;
 
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.jumpmind.pos.core.ui.messagepart.PromptButtonRowPart;
 
 @Getter
 @Setter
@@ -16,8 +18,6 @@ import lombok.Setter;
 public class GiftReceiptUnmaskCodeDialogUIMessage extends UIMessage {
     public static final String GIFT_RECEIPT_UNMASK_CODE_WINDOW_ID = "GiftCodeChecker";
 
-    private String closeText;
-    private String closeAction;
     private String enterCodeLabel;
     private String enterGiftCode;
     private String showValue;
@@ -26,6 +26,27 @@ public class GiftReceiptUnmaskCodeDialogUIMessage extends UIMessage {
     private String currencySymbol;
     private String incorrectResultValue;
     private String unmaskCode;
+    private String placeholder;
+    private boolean autofocus;
+    private PromptButtonRowPart promptButtonRow;
+
+    public void setButtons(List<ActionItem> buttons) {
+        if (buttons != null && buttons.size() > 0) {
+            promptButtonRow.setPrimaryButton(buttons.get(0));
+
+            for (int b = 1; b < buttons.size(); b++) {
+                promptButtonRow.addSecondaryButton(buttons.get(b));
+            }
+        }
+    }
+
+    public void addButton(ActionItem button) {
+        if (promptButtonRow.getPrimaryButton() == null) {
+            promptButtonRow.setPrimaryButton(button);
+        } else {
+            promptButtonRow.addSecondaryButton(button);
+        }
+    }
 
     @Override
     public String getScreenType() {
