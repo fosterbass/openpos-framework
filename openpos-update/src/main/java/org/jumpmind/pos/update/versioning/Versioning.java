e31ec3f91ba9916bbcac5f60ec601827b729c36d
Kevin DeLorey <kevin.delorey@leadingedje.com>
2021-10-04 13:25:07 -0600

    Initial commit of the downloader job.

--- openpos-update/src/main/java/org/jumpmind/pos/update/versioning/Versioning.java
+++ openpos-update/src/main/java/org/jumpmind/pos/update/versioning/Versioning.java
@@ -11,7 +11,7 @@ import java.util.Map;
 @Component
 @Slf4j
 public class Versioning {
-    @Value("${openpos.update.versioning.provider}")
+    @Value("${openpos.update.versioning}")
     String versioningProvider;
 
     @Autowired
