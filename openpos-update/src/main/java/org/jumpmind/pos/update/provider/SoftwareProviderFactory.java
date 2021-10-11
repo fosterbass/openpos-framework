a3df07b75064b7e752dfdeb69b0475f08a6087a2
Dan Kaste <dan.kaste@leadingedje.com>
2021-10-11 13:33:46 -0400

    Add logging

--- openpos-update/src/main/java/org/jumpmind/pos/update/provider/SoftwareProviderFactory.java
+++ openpos-update/src/main/java/org/jumpmind/pos/update/provider/SoftwareProviderFactory.java
@@ -1,11 +1,13 @@
 package org.jumpmind.pos.update.provider;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 public class SoftwareProviderFactory {
 
     @Value("${openpos.update.softwareProvider:fileSystemSoftwareProvider}")
@@ -15,6 +17,7 @@ public class SoftwareProviderFactory {
     ApplicationContext applicationContext;
     
     public ISoftwareProvider getSoftwareProvider() {
+        log.info("Using " + softwareProvider);
         return applicationContext.getBean(softwareProvider, ISoftwareProvider.class);
     }
     
