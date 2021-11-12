75f6b0df511d65417e611f8afa61b1a0c86b3a0e
Brandon Lewis <blewis@manifestcorp.com>
2021-11-12 11:23:22 -0500

    Adjustments to get Petco onto master (#1743)
    * adjustments to get the 1.13 branch to compile and build against commerce master branch. 
    
    Co-authored-by: Robert Cochran <rcochran@manifestcorp.com>

--- openpos-util/src/main/java/org/jumpmind/pos/util/model/PrintMessage.java
+++ openpos-util/src/main/java/org/jumpmind/pos/util/model/PrintMessage.java
@@ -2,6 +2,7 @@ package org.jumpmind.pos.util.model;
 
 public class PrintMessage extends Message {
     String html;
+    String printerId;
 
     public PrintMessage() {
         super("Print");
@@ -12,11 +13,25 @@ public class PrintMessage extends Message {
         this.html = html;
     }
 
+    public PrintMessage(String html, String printerId) {
+        super("Print");
+        this.html = html;
+        this.printerId = printerId;
+    }
+
     public void setHtml(String html) {
         this.html = html;
     }
 
     public String getHtml() {
         return html;
+    }
+
+    public void setPrinterId(String printerId) {
+        this.printerId = printerId;
+    }
+
+    public String getPrinterId() {
+        return printerId;
     }
 }
