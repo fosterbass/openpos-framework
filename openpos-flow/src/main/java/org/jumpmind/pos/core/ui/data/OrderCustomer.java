be2be5a05a7b50343c2b1e4660e1506726aa1841
Klementina S. Chirico <kstojanovska@jumpmind.com>
2021-11-24 11:11:07 -0500

    Feature/order summary updates (#1768)
    * update infinite-scroll component to support horizontal orientation
    
    * updateupdate OrderSummary to add orderType and change iconName to be orderTypeIcon. Additionally use lombok instead of getters/setters
    
    * add bopis, curbside, and ship icon svgs
    
    * change input name from scrollOrientation to orientation
    
    * update infinite-scroll
    
    * add a priority to orderSummary
    
    * update OrderSummary to organize fields and use lombok AllArgsConstructor and NoArgsConstructor
    
    * review feedback
    
    * update OrderCustomer to use @AllArgsConstructor instead of typing out the constructor & remove no longer needed constructor.
    
    * add statusCode to orderSummary

--- openpos-flow/src/main/java/org/jumpmind/pos/core/ui/data/OrderCustomer.java
+++ openpos-flow/src/main/java/org/jumpmind/pos/core/ui/data/OrderCustomer.java
@@ -1,35 +1,20 @@
 package org.jumpmind.pos.core.ui.data;
 
-import java.io.Serializable;
-
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+
 @Builder
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class OrderCustomer implements Serializable {
     private String iconName;
     private String fullName;
     private String phone;
     private String email;
     private Address address;
-
-    public OrderCustomer(String iconName, String firstName, String lastName, String phone, String email, String line1, String city, String state, String postalCode) {
-        this.fullName = firstName.concat(" ").concat(lastName);
-        this.phone = phone;
-        this.email = email;
-        this.address = new Address(line1, city, state, postalCode);
-    }
-
-    public OrderCustomer(String iconName, String fullName, String phone, String email, Address address) {
-        this.iconName = iconName;
-        this.fullName = fullName;
-        this.phone = phone;
-        this.email = email;
-        this.address = address;
-    }
-
-
 }
