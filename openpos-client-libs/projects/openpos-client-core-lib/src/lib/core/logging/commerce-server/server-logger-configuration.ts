14fdf1ae79644c3f67a73737ae84502b1c69fc3a
Eddie Legg <74971030+eddielegg-le@users.noreply.github.com>
2022-01-27 11:58:04 -0500

    server logger enabled flag sent as string (#1946)
    * server logger enabled flag sent as string
    
    * Change to string

--- openpos-client-libs/projects/openpos-client-core-lib/src/lib/core/logging/commerce-server/server-logger-configuration.ts
+++ openpos-client-libs/projects/openpos-client-core-lib/src/lib/core/logging/commerce-server/server-logger-configuration.ts
@@ -1,7 +1,7 @@
 import { ConfigChangedMessage } from '../../messages/config-changed-message';
 
 export class ServerLoggerConfiguration extends ConfigChangedMessage {
-    constructor(public enabled: boolean, public logBufferTime?: number) {
+    constructor(public enabled: string, public logBufferTime?: number) {
         super('server-logger');
     }
 }
