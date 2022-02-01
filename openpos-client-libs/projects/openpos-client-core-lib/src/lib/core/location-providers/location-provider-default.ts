d45036b5f7d32ea87110100aba63be74f25c3821
Jason Mihalick <jrmihalick@users.noreply.github.com>
2022-02-01 09:49:51 -0500

    PDPOS-7330: Send location data within 5 secs getting google api key (#1959)
    In deployments we have seen delays before geolocation.watchPosition is invoked.  Sometimes the device can be used for hours before a watchPosition callback occurs.
    
    Alters the getLocation logic as follows:
    - Instead of calling geolocation.watchPosition and waiting for the callback, attempt to invoke the geolocation.getCurrentPosition() API as soon as we have the googleApiKey and position offset parameters
    - getCurrentPosition() may not work in all circumstances and, in particular, requires a secure https connection to work.  The browser may also not allow it and fail due to the "[Violation] Only request geolocation information in response to a user gesture" error.
    - If getCurrentPosition fails, we fall back to calling the google geolocation API (web service) to get the current location.  This should succeed in just about all scenarios.
    - If the google geolocation API fails, then I set the lat/long to 0,0 and let the current mechanism of depending on watchPosition to run.
    - If getCurrentPosition is successful, then watchPosition will be invoked for to continue watching for position changes.

--- openpos-client-libs/projects/openpos-client-core-lib/src/lib/core/location-providers/location-provider-default.ts
+++ openpos-client-libs/projects/openpos-client-core-lib/src/lib/core/location-providers/location-provider-default.ts
@@ -1,12 +1,12 @@
 import { ILocationProvider } from './location-provider.interface';
 import { ILocationData } from './location-data.interface';
-import { Observable } from 'rxjs/internal/Observable';
+import { Observable, of, Subject } from 'rxjs';
 import { Http } from '@angular/http';
 import { Configuration } from '../../configuration/configuration';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import {ConfigurationService} from "../services/configuration.service";
-import {filter, take} from "rxjs/operators";
+import { catchError, filter, take, timeout } from 'rxjs/operators';
 
 @Injectable({
     providedIn: 'root'
@@ -33,51 +33,131 @@ export class LocationProviderDefault implements ILocationProvider {
         this.coordinateBuffer = buffer;
         console.info(`[LocationProviderDefault] navigator.geolocation: ${navigator.geolocation}, CONFIGURATION.googleApiKey: ${Configuration.googleApiKey}, googleApiKey: ${googleApiKey}`);
         if (navigator.geolocation && (Configuration.googleApiKey || googleApiKey)) {
-            let zipCode = '';
-            let  countryName = '';
-            const previous = {latitude: 0, longitude: 0};
-            navigator.geolocation.watchPosition((position) => {
-                const lat = position.coords.latitude;
-                const long = position.coords.longitude;
-                if (lat > previous.latitude + this.coordinateBuffer || lat < previous.latitude - this.coordinateBuffer
-                    || long > previous.longitude + this.coordinateBuffer || long < previous.longitude - this.coordinateBuffer) {
-                    previous.latitude = lat;
-                    previous.longitude = long;
-                    const latlong = lat + ',' + long;
-                    console.log(`[LocationProviderDefault] Calling google maps geocode api with lat,long: ${latlong}`);
-                    this.reverseGeocode(Configuration.googleApiKey, latlong)
-                        .then((response) => {
-                            console.log(`[LocationProviderDefault] Geocode response: ${response.results[0].address_components}`);
-                            for (const addressComponent of response.results[0].address_components) {
-                                for (const type of addressComponent.types) {
-                                    if (type === 'postal_code') {
-                                        zipCode = addressComponent.long_name;
-                                    }
-                                    if (type === 'country') {
-                                        countryName = addressComponent.long_name;
-                                    }
-                                }
-                            }
+            console.info(`[LocationProviderDefault] Attempting to get current GPS position...`);
+            const curPosition$ = new Subject<{latitude: number, longitude: number}>();
+            this.getCurrentPosition(Configuration.googleApiKey, {timeout: 5000}).then(async (initPosition) => {
+                console.info(`[LocationProviderDefault] Got initial position: ${initPosition}`);
 
-                            const data = {
-                                type: 'default',
-                                postalCode: zipCode,
-                                latitude: lat.toString(),
-                                longitude: long.toString(),
-                                country: countryName
-                            } as ILocationData;
-                            console.info(`[LocationProviderDefault] new locationData: ${JSON.stringify(data)}`);
-                            this.$locationData.next(data);
-                        })
-                        .catch((error) => console.log(error));
+                if (initPosition != null) {
+                    const initPos = {latitude: initPosition.coords.latitude, longitude: initPosition.coords.longitude};
+                    await this.invokeGoogleMapsApiAndEmitLocation(Configuration.googleApiKey, initPos.latitude, initPos.longitude);
+                    curPosition$.next(initPos);
+                } else {
+                    console.warn(`[LocationProviderDefault] All methods to get an initial position failed.`);
+                    curPosition$.next({latitude: 0, longitude: 0});
                 }
+            },
+            err => {
+                console.warn(`[LocationProviderDefault] Error getting initial position: ${JSON.stringify(err)}`);
+                curPosition$.next({latitude: 0, longitude: 0});
             });
+
+            curPosition$.subscribe( previousPos => {
+                console.info(`[LocationProviderDefault] Now starting geolocation.watchPosition with previousPos: ${JSON.stringify(previousPos)}`);
+                navigator.geolocation.watchPosition((position) => {
+                    const lat = position.coords.latitude;
+                    const long = position.coords.longitude;
+                    if (lat > previousPos.latitude + this.coordinateBuffer || lat < previousPos.latitude - this.coordinateBuffer
+                        || long > previousPos.longitude + this.coordinateBuffer || long < previousPos.longitude - this.coordinateBuffer) {
+                        previousPos.latitude = lat;
+                        previousPos.longitude = long;
+
+                        this.invokeGoogleMapsApiAndEmitLocation(Configuration.googleApiKey, lat, long);
+                    }
+                });
+            });
+
         }
         return this.$locationData;
     }
 
+    private getCurrentPosition(googleApiKey: string, options: PositionOptions): Promise<Position> {
+        return new Promise((resolve, reject) =>
+            navigator.geolocation.getCurrentPosition(
+                resolve,
+                (err) => {
+                    console.warn(`[LocationProviderDefault] getCurrentPosition failed, falling back to calling google. Error: ${JSON.stringify(err)}`);
+                    resolve(this.getGoogleAPILocation(googleApiKey));
+                },
+                options
+            )
+        );
+    }
+
+    private async getGoogleAPILocation(googleApiKey: string): Promise<Position> {
+        console.log(`[LocationProviderDefault] Calling google location api to get location`);
+        const response = await this.http
+            .post(`https://www.googleapis.com/geolocation/v1/geolocate?key=${googleApiKey}`, {})
+            .pipe(
+                timeout(3000),
+                catchError(e => {
+                    console.warn(`[LocationProviderDefault] Google location api failed with error: ${JSON.stringify(e)}`);
+                    return of(null);
+                })
+            ).toPromise();
+        if (response != null) {
+            const result = response.json();
+            console.info(`[LocationProviderDefault] Google location api returned: ${JSON.stringify(result)}`);
+            if (result.location) {
+                return {
+                    coords: {
+                        latitude: result.location.lat,
+                        longitude: result.location.lng,
+                        accuracy: result.accuracy,
+                        altitude: null,
+                        altitudeAccuracy: null,
+                        heading: null,
+                        speed: null
+                    },
+                    timestamp: Date.now()
+                };
+            } else {
+                console.warn(`[LocationProviderDefault] No location found on Google location api result: ${JSON.stringify(result)}`);
+                return null;
+            }
+        } else {
+            console.warn(`[LocationProviderDefault] null response from Google location api`);
+            return null;
+        }
+    }
+
+    private async invokeGoogleMapsApiAndEmitLocation(apiKey: string, lat: number, long: number) {
+        const latlong = lat + ',' + long;
+        let zipCode = '';
+        let  countryName = '';
+
+        await this.reverseGeocode(apiKey, latlong)
+            .then((response) => {
+                console.log(`[LocationProviderDefault] Geocode response: ${response.results[0].address_components}`);
+                for (const addressComponent of response.results[0].address_components) {
+                    for (const type of addressComponent.types) {
+                        if (type === 'postal_code') {
+                            zipCode = addressComponent.long_name;
+                        }
+                        if (type === 'country') {
+                            countryName = addressComponent.long_name;
+                        }
+                    }
+                }
+
+                const data = {
+                    type: 'default',
+                    postalCode: zipCode,
+                    latitude: lat.toString(),
+                    longitude: long.toString(),
+                    country: countryName
+                } as ILocationData;
+                console.info(`[LocationProviderDefault] new locationData: ${JSON.stringify(data)}`);
+                this.$locationData.next(data);
+            })
+            .catch((error) => console.log(error));
+
+
+    }
+
     async reverseGeocode(key: string, param: string): Promise<any> {
         try {
+            console.log(`[LocationProviderDefault] Calling google maps geocode api with lat,long: ${param}`);
             const response = await this.http
                 .get('https://maps.google.com/maps/api/geocode/json?key=' + key + '&latlng=' + param + '&sensor=false')
                 .toPromise();
