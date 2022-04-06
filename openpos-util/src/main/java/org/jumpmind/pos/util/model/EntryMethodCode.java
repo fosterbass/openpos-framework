6c35bc4c6906bddf5c3495023b652d994d4f785c
Jason Mihalick <jrmihalick@users.noreply.github.com>
2022-04-06 17:39:23 -0400

    #2164: Prepare for eventual move of EntryMethodCode to openpos. (#2165)
    - Creating a copy of the class here so that new code after release 3.0 can use it instead of `org.jumpmind.pos.sales.model.EntryMethodCode`

---
+++ openpos-util/src/main/java/org/jumpmind/pos/util/model/EntryMethodCode.java
@@ -0,0 +1,33 @@
+package org.jumpmind.pos.util.model;
+
+public class EntryMethodCode extends AbstractTypeCode {
+    private static final long serialVersionUID = 1L;
+    private static final String[] DESERIALIZE_SEARCH_CLASSES = {
+            "org.jumpmind.pos.sales.model.EntryMethodCode",
+            "org.jumpmind.pos.item.model.EntryMethodCode",
+            EntryMethodCode.class.getName()
+    };
+
+    public static final EntryMethodCode CUSTOMER_DEVICE =     new EntryMethodCode("CUSTOMER_DEVICE");
+    public static final EntryMethodCode SCANNED =     new EntryMethodCode("SCANNED");
+    public static final EntryMethodCode KEYED =       new EntryMethodCode("KEYED");
+    public static final EntryMethodCode INQUIRY =     new EntryMethodCode("INQUIRY");
+    public static final EntryMethodCode TRANSACTION = new EntryMethodCode("TRANSACTION");
+    public static final EntryMethodCode OTHER =       new EntryMethodCode("OTHER");
+    public static final EntryMethodCode SYSTEM =      new EntryMethodCode("SYSTEM");
+
+    public static EntryMethodCode of(String value) {
+        return AbstractTypeCode.of(EntryMethodCode.class, value);
+    }
+    
+    private EntryMethodCode(String value) {
+        super(value);
+    }
+
+    @Override
+    public String[] getDeserializationSearchClasses() {
+        return DESERIALIZE_SEARCH_CLASSES;
+    }
+
+}
+
