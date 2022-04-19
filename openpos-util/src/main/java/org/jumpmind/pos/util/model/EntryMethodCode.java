dc861d606d9f3b6fe2f5eb19f4815b87eb624353
Joe Chirico <chiricoj@ae.com>
2022-04-19 10:45:21 -0400

    Fix serialization of EntryMethodCode

--- openpos-util/src/main/java/org/jumpmind/pos/util/model/EntryMethodCode.java
+++ openpos-util/src/main/java/org/jumpmind/pos/util/model/EntryMethodCode.java
@@ -3,7 +3,6 @@ package org.jumpmind.pos.util.model;
 public class EntryMethodCode extends AbstractTypeCode {
     private static final long serialVersionUID = 1L;
     private static final String[] DESERIALIZE_SEARCH_CLASSES = {
-            "org.jumpmind.pos.sales.model.EntryMethodCode",
             "org.jumpmind.pos.item.model.EntryMethodCode",
             EntryMethodCode.class.getName()
     };
