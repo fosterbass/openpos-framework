1a5de763cef09d6f5ff61f4bc540d608d6e253ac
jaeleeLogic <104446137+jaeleeLogic@users.noreply.github.com>
2022-05-12 10:56:57 -0500

    Create effectiveEndVersion by refactoring EffectiveVersion.java to EffectiveAndEndVersion.java #6447 (#2227)
    Provide an effectiveEndVersion to bookend effectiveVersion on configuration tables. We need to be able to remove buttons with specific versions as well as add them

--- openpos-persist/src/main/java/org/jumpmind/pos/persist/model/EffectiveAndEndVersion.java
+++ openpos-persist/src/main/java/org/jumpmind/pos/persist/model/EffectiveAndEndVersion.java
@@ -8,9 +8,11 @@ import java.io.Serializable;
 
 @Getter
 @Setter
-public class EffectiveVersion implements Serializable {
+public class EffectiveAndEndVersion implements Serializable {
 
     @ColumnDef(description = "Software Version at which this data should become available", defaultValue = "0")
     int effectiveVersion;  // NOSONAR
 
+    @ColumnDef(description = "Software Version at which any version after this data will expire and not become")
+    int effectiveEndVersion;  // NOSONAR
 }
