377bcb8e7bddd10552a718c27d1a7f951db0cc02
RStellaRobin <89908480+RStellaRobin@users.noreply.github.com>
2022-06-07 15:34:18 +0530

    Removal of F12 [function keys] for VC enrollment
    When press F12 Fn key from partner keyboard in Membership details screen, system Not responded which is assigned to Enroll another Pet

--- openpos-flow/src/main/java/org/jumpmind/pos/core/ui/message/CustomerMembershipsUIMessage.java
+++ openpos-flow/src/main/java/org/jumpmind/pos/core/ui/message/CustomerMembershipsUIMessage.java
@@ -8,7 +8,6 @@ import org.jumpmind.pos.core.ui.UIMessage;
 import java.util.List;
 
 @Data
-@AssignKeyBindings
 public class CustomerMembershipsUIMessage extends UIMessage {
     private static final long serialVersionUID = 1L;
 
