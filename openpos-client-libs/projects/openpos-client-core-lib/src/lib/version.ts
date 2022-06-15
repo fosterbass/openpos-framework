db88f190e03f6a1f9068464f25ea341833eb7134
Mitchell Ruffing <36873630+MRUFFING67@users.noreply.github.com>
2022-06-15 18:47:15 -0400

    Adapting from Azure to Jenkins
    - added jenkinsfile
    - updated some dependencies
    - updated publishing

--- openpos-client-libs/projects/openpos-client-core-lib/src/lib/version.ts
+++ openpos-client-libs/projects/openpos-client-core-lib/src/lib/version.ts
@@ -1,4 +1,4 @@
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
-export const VERSION = {"componentName":"openpos-client-core-lib","version":"0.4.7-urbndev-SNAPSHOT","gitHash":"d1995bf0c942d21c21cd5ea464cd42726df329c8","buildTime":"2020-03-18 12:42:39","gitBranch":"urbn/0.4-dev"};
+export const VERSION = {"componentName":"openpos-client-core-lib","version":"0.5.0-urbndev-SNAPSHOT","gitHash":"c4ce15c2e92096e81efcf2b9da3fd8c6da041b0d","buildTime":"2022-06-15 22:44:32","gitBranch":"releases/urbn/0.5-dev"};
 /* tslint:enable */
