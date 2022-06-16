4ce7fc79c904e3d5e9399a3555f984bb643f545d
Mitchell Ruffing <36873630+MRUFFING67@users.noreply.github.com>
2022-06-16 15:58:48 -0400

    Adapting from Azure to Jenkins (#2309)
    * Adapting from Azure to Jenkins
    
    - added jenkinsfile
    - updated some dependencies
    - updated publishing
    
    * Update task names
    
    - Copying task file (build.gradle) from releases/urbn/0.5 branch
    
    * Update lombok dependency in java.gradle
    
    With updated groovy and gradle, the way you specify the dependency changes

--- openpos-client-libs/projects/openpos-client-core-lib/src/lib/version.ts
+++ openpos-client-libs/projects/openpos-client-core-lib/src/lib/version.ts
@@ -1,4 +1,4 @@
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
-export const VERSION = {"componentName":"openpos-client-core-lib","version":"0.4.15-SNAPSHOT","gitHash":"9ebabe8cdc5d5a9cdb88142cb4962e49e7c98fda","buildTime":"2020-05-22 14:33:34","gitBranch":"TS-8118-no-receipt-return-policy-changes"};
+export const VERSION = {"componentName":"openpos-client-core-lib","version":"0.4.23-SNAPSHOT","gitHash":"3a9b7f55f3f98117c5ab2711ce72d39543ce9bdc","buildTime":"2022-06-16 19:50:16","gitBranch":"feature/Adapt-From-Azure-To-Jenkins"};
 /* tslint:enable */
