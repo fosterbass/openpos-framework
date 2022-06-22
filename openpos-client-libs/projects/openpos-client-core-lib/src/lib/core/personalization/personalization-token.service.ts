359c819520a54a3c6b3b1302d36e1d2d87de95d7
Mark Michalek <mmichalek@jumpmind.com>
2022-06-22 19:06:39 -0400

    Support non-api server URL for static images

--- openpos-client-libs/projects/openpos-client-core-lib/src/lib/core/personalization/personalization-token.service.ts
+++ openpos-client-libs/projects/openpos-client-core-lib/src/lib/core/personalization/personalization-token.service.ts
@@ -7,6 +7,7 @@ import { PersonalizationService } from './personalization.service';
 })
 export class PersonalizationTokenService {
     static readonly BASE_URL_TOKEN = '${apiServerBaseUrl}';
+    static readonly BASE_SERVER_URL_TOKEN = '${serverBaseUrl}';
     static readonly APP_ID_TOKEN = '${appId}';
     static readonly DEVICE_ID_TOKEN = '${deviceId}';
 
@@ -19,10 +20,12 @@ export class PersonalizationTokenService {
         }
 
         const apiServerBaseUrl = this.discoveryService.getApiServerBaseURL();
+        const serverBaseUrl = this.discoveryService.getServerBaseURL();
         const deviceId = this.personalizationService.getDeviceId$().getValue();
         const appId = this.personalizationService.getAppId$().getValue();
 
         let url = value.replace(PersonalizationTokenService.BASE_URL_TOKEN, apiServerBaseUrl);
+        url = url.replace(PersonalizationTokenService.BASE_SERVER_URL_TOKEN, serverBaseUrl);
         url = url.replace(PersonalizationTokenService.APP_ID_TOKEN, appId);
 
         return url.replace(PersonalizationTokenService.DEVICE_ID_TOKEN, deviceId);
